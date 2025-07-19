2fb51198b470ba07f0de6baf98b08bfd9ee8ab02
WeskleyReis <weskleyff88004216@gmail.com>
2025-07-18 21:39:02 -0300

    Estilizando o CSS, corrigindo bugs na validação do favicon, criando templates necessários para o layout

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(
         request,
         'blog/pages/index.html',
+        {
+            'nome': 'Luiz Otávio'
+        }
+    )
+
+
+def page(request):
+    return render(
+        request,
+        'blog/pages/page.html',
+        {
+            'nome': 'Luiz Otávio'
+        }
+    )
+
+
+def post(request):
+    return render(
+        request,
+        'blog/pages/post.html',
         {
             'nome': 'Luiz Otávio'
         }
--- djangoapp/utils/model_validators.py
+++ djangoapp/utils/model_validators.py
@@ -2,5 +2,5 @@ from django.core.exceptions import ValidationError
 
 
 def validate_png(image):
-    if not image.name.lower().endswitch('.png'):
+    if not image.name.lower().endswith('.png'):
         raise ValidationError('Imagem precisa ser no formato .png!')
