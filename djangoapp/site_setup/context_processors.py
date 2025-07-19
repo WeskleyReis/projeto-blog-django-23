1ece8738bc85af738e44a87506858c21388dae18
WeskleyReis <weskleyff88004216@gmail.com>
2025-07-18 22:42:32 -0300

    #

--- djangoapp/site_setup/context_processors.py
+++ djangoapp/site_setup/context_processors.py
@@ -1,12 +1,6 @@
 from site_setup.models import SiteSetup
 
 
-def context_processor_example(request):
-    return {
-        'example' : 'Veio do context processor (example)'
-    }
-
-
 def site_setup(request):
     setup = SiteSetup.objects.order_by('-id').first()
 
