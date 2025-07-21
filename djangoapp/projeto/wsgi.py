73ead9bab95846c61a8d7f8156c96179f135fdd7
WeskleyReis <weskleyff88004216@gmail.com>
2025-07-21 20:46:59 -0300

    Projeto final v1.0.0

--- djangoapp/projeto/wsgi.py
+++ djangoapp/projeto/wsgi.py
@@ -11,6 +11,14 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# DOTENV
+load_dotenv(BASE_DIR.parent / 'dotenv_files' / '.env', override=True)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto.settings')
 
 application = get_wsgi_application()
